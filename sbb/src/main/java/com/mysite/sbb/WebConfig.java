3c1a7548e425283d973ae9f03c943b07046d1e9e
Jinsoo Park <jinny.park908@gmail.com>
2024-08-12 18:06:21 +0900

    fix: cloudsql 연결

---
+++ sbb/src/main/java/com/mysite/sbb/WebConfig.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // 허용할 도메인 추가
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 자격 증명을 포함한 요청 허용
+    }
+}
